7a38291cb77023698b38ae56ff7c80756ae181a4
BigChungus1924 <116966046+BigChungus1924@users.noreply.github.com>
2022-11-03 15:48:02 +0100

    Add files via upload

--- install.sh
+++ install.sh
@@ -19,9 +19,11 @@ cp Orchis-Dark-Compact /home/$username/.themes
 cp config  /home/$username/.config/i3
 cp alacritty.yml /home/$username/.config/alacritty
 
-sudo apt install i3 xorg compton nitrogen polybar conky neofetch pcmanfm network-manager nm-tray lxpolkit meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+sudo apt install i3 chromium network-manager nm-applet nmcli blueberry brightlight gimp blender kdenlive mousepad dunst htop pcmanfm neofetch lxappearence feh polybar xorg compton cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 
 nitrogen /home/$username/Pictures/wallpaper.jpg
+
+
 
 cd /home/$username/
 
@@ -39,12 +41,5 @@ sudo cp extra/logo/alacritty-term.svg /usr/share/pixmaps/Alacritty.svg
 sudo desktop-file-install extra/linux/Alacritty.desktop
 sudo update-desktop-database
 
-sudo apt update && sudo apt install -y gnupg
-gpg --keyserver keyserver.ubuntu.com --recv 0xfaf1020699503176
-gpg --export 0xfaf1020699503176 | sudo tee /usr/share/keyrings/ulauncher-archive-keyring.gpg
-sudo echo "deb [signed-by=/usr/share/keyrings/ulauncher-archive-keyring.gpg] \
-          http://ppa.launchpad.net/agornostal/ulauncher/ubuntu focal main" \
-          > /etc/apt/sources.list.d/ulauncher-focal.list
-sudo apt update && sudo apt install ulauncher
 
 
