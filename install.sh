dc5b0b7591b9ffe2ca6971f85de900f9399a909c
BigChungus1924 <116966046+BigChungus1924@users.noreply.github.com>
2022-11-03 11:39:57 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -19,16 +19,9 @@ cp Orchis-Dark-Compact /home/$username/.themes
 cp config  /home/$username/.config/i3
 cp alacritty.yml /home/$username/.config/alacritty
 
-sudo apt install xorg compton nitrogen polybar conky neofetch pcmanfm network-manager nm-tray lxpolkit meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
+sudo apt install i3 xorg compton nitrogen polybar conky neofetch pcmanfm network-manager nm-tray lxpolkit meson dh-autoreconf libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev xcb libxcb1-dev libxcb-icccm4-dev libyajl-dev libev-dev libxcb-xkb-dev libxcb-cursor-dev libxkbcommon-dev libxcb-xinerama0-dev libxkbcommon-x11-dev libstartup-notification0-dev libxcb-randr0-dev libxcb-xrm0 libxcb-xrm-dev libxcb-shape0 libxcb-shape0-dev cmake pkg-config libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev python3
 
 nitrogen /home/$username/Pictures/wallpaper.jpg
-
-git clone https://github.com/Airblader/i3 i3-gaps
-cd i3-gaps
-mkdir -p build && cd build
-meson --prefix /usr/local
-ninja
-sudo ninja install
 
 cd /home/$username/
 
